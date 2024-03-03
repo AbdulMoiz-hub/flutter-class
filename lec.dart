7101739e830da4bbc5ee3f9b0f21fc277b5d7714
AbdulMoiz-hub <howtodo0147@gmail.com>
2024-03-03 15:51:41 +0500

    Git Github lecture

---
+++ lec.dart
@@ -0,0 +1,26 @@
+void main (){
+  print('abc');
+
+  /*
+    1) Git ko start karne k lea
+      git init
+
+    2) Git m files dalne k lea
+      git add .
+
+    3) Git m files ko save karne k lea ya jo kaam change kia hai usse save karne k lea
+      git commit -m [message]
+
+      yaha par [message] koi bhi message jese is me mene ye wala kaam kia hai
+
+    4) Git me jitna bhi kaam hai usko ab Github m daalna k lea (Repository me) 
+       Abhi Github ki Repository ko apne pc k git k folder se connnect karne ke lea
+
+      git branch -M main
+      git remote add origin https://github.com/AbdulMoiz-hub/flutter-class.git
+
+    5) Git me jitna bhi kaam hai usko ab Github m daalna k lea (Repository me)
+      git push -u origin main
+    
+  */
+}
