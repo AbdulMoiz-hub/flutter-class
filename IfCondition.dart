ecf1a83b8c1940c67aaaad57e2bd775fc9599d04
AbdulMoiz-hub <howtodo0147@gmail.com>
2024-03-10 14:15:26 +0500

    Task Completed

--- IfCondition.dart
+++ IfCondition.dart
@@ -1,3 +1,21 @@
 void main(){
-  
+  /*
+  =============== Task ==============
+  int math = 90;
+  int physics = 90;
+  int english = 90;
+
+  int obtainedMarks = math + physics + english;
+  int total = 300;
+  double percentage = (obtainedMarks / total) * 100;
+  if(percentage > 95){
+    print('A+');
+  }
+  else if(percentage > 85){
+    print('A');
+  }
+  else{
+    print('B');
+  }
+  */
 }
